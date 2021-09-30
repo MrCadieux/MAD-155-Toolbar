51d0096e0a736b217b823285dbb152d12fc6a2f3
MrCadieux <jcadieux@d155.org>
2021-09-30 07:01:38 -0500

    Learning how to create and use a Toolbar

---
+++ app/src/main/java/com/example/mytoolbar3/Profile.kt
@@ -0,0 +1,50 @@
+package com.example.mytoolbar3
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+
+class Profile : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+
+        setSupportActionBar(findViewById(R.id.my_toolbar))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.actionbuttons,menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId){
+            R.id.action_favorite -> {
+                Toast.makeText(this, "Favorite Selected", Toast.LENGTH_SHORT).show()
+                intent = Intent(this, Favorite::class.java)
+                startActivity(intent)
+            }
+            R.id.action_settings -> {
+                Toast.makeText(this, "Settings Selected", Toast.LENGTH_SHORT).show()
+                intent = Intent(this, Settings::class.java)
+                //startActivity(intent)
+            }
+            R.id.action_games -> {
+                Toast.makeText(this, "Games Selected", Toast.LENGTH_SHORT).show()
+                intent = Intent(this, Games::class.java)
+                startActivity(intent)
+            }
+            R.id.action_profile -> {
+                Toast.makeText(this, "Profile Selected", Toast.LENGTH_SHORT).show()
+                intent = Intent(this, Profile::class.java)
+                startActivity(intent)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
